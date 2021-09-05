fdab568abe4a4601819edd79b3cee46c6b4160f3
Gentle-Hui <1120365945@qq.com>
2021-09-05 21:48:39 +0800

    3

---
+++ C SHARP学习第一天/StudyExperience0904/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyExperience0904
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "123";
+            //强制类型转换
+            double d = Convert.ToDouble(str);
+            //转什么，to什么
+            Console.WriteLine("字符串被转换为{0}.", d);
+            Console.ReadKey();
+        }
+    }
+}
