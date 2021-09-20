79f15f72b1023019c2c3d6452ed3338c3770fe35
Gentle-Hui <1120365945@qq.com>
2021-09-20 09:54:02 +0800

    20

---
+++ C SHARP学习第一天/StudyExperience0918/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyExperience0918
+{
+    public class Person
+    {
+        //字段存储数据
+        string _name;
+        int _age;
+        char _gender;
+
+        public char Gender1
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+        //属性控制数据
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Age { set { _age = value; } get { return _age; } }
+        public char Gender { set { _gender = value; } get { return _gender; } }
+
+        //方法使用数据
+        public void PersionalData (){
+            Console.WriteLine("{0}同学是{1}生，今年{2}岁了",this.Name,this.Gender,this.Age);
+        }
+    }
+}
---
+++ C SHARP学习第一天/StudyExperience0918/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyExperience0918
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //将对象实例化
+            Person data = new Person();
+            data.Age = 23;
+            data.Gender='男';
+            data.Name = "张三";
+            data.PersionalData();
+            Console.ReadKey();
+        }
+    }
+}
